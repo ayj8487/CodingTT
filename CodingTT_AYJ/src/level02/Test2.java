94dd531fb0bc863cd0b699edbe992d8827c5fe2c
ayj8487 <ayj8487@naver.com>
2021-10-31 23:25:36 +0900

    baekjoon 02_02_9498(com)

--- CodingTT_AYJ/src/level02/Test2.java
+++ CodingTT_AYJ/src/level02/Test2.java
@@ -8,10 +8,20 @@ public class Test2 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int a,b ; 
+		int a ; 
 		a = sc.nextInt();
-		b = sc.nextInt();
 
+		if(a >= 90) {
+			System.out.println("A");
+		}else if(a >=80) {
+			System.out.println("B");
+		}else if (a >=70) {
+			System.out.println("C");
+		}else if (a >=60){
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
 	
 	}
 
---
+++ CodingTT_AYJ/src/level02/Test3.java
@@ -0,0 +1,17 @@
+package level02;
+
+import java.util.Scanner;
+
+public class Test3 {
+
+	//백준코딩 단계2 if문 03문제 2753
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a ; 
+		a = sc.nextInt();
+
+	
+	}
+
+}
