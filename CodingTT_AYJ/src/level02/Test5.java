b7fdd8b4da1ae5e981be69159c46ad48979f6044
ayj8487 <ayj8487@naver.com>
2021-11-29 03:14:16 +0900

    baekjoon 02_05_2884(com)

--- CodingTT_AYJ/src/level02/Test5.java
+++ CodingTT_AYJ/src/level02/Test5.java
@@ -4,27 +4,25 @@ import java.util.Scanner;
 
 public class Test5 {
 
-	//백준코딩 단계2 if문 04문제 14681
+	//백준코딩 단계2 if문 05문제 2884
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		 int a = s.nextInt();
-         int b = s.nextInt();
-         int c=0;
-        if(a>0) {
-            if(b>0){
-                c=1;
-            }else{
-                c=4;
+		  int H = s.nextInt(); //시
+		  int M = s.nextInt(); //분
+		
+        if(M < 45){
+            M = 60-(45-M);
+            if(H > 0){
+                H--; 
+            }else {
+                H = 23;
             }
+            System.out.println(H + " " + M);
         }else{
-            if(b>0){
-                c=2;
-            }else{
-                c=3;
-            }
+            M = M-45;
+            System.out.println(H + " " + M);
         }
-        System.out.println(c);
 	}
 }
 
---
+++ CodingTT_AYJ/src/level03/Test1.java
@@ -0,0 +1,13 @@
+package level03;
+
+
+public class Test1 {
+
+	//백준코딩 단계3 for문 01문제 2739
+	public static void main(String[] args) {
+
+	
+		
+	}
+	}
+
