e5b43893606e74b4779a7de810d1b850b0695f74
ayj8487 <ayj8487@naver.com>
2021-11-28 21:30:03 +0900

    baekjoon 02_04_14681(com)

--- CodingTT_AYJ/src/level02/Test3.java
+++ CodingTT_AYJ/src/level02/Test3.java
@@ -6,12 +6,15 @@ public class Test3 {
 
 	//백준코딩 단계2 if문 03문제 2753
 	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
 
-		Scanner sc = new Scanner(System.in);
-		int a ; 
-		a = sc.nextInt();
-
-	
-	}
-
+		int A = scanner.nextInt();
+		
+		if (A % 4 == 0 && (A % 100 != 0 || A % 400 == 0)) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+    }
 }
---
+++ CodingTT_AYJ/src/level02/Test4.java
@@ -0,0 +1,30 @@
+package level02;
+
+import java.util.Scanner;
+
+public class Test4 {
+
+	//백준코딩 단계2 if문 04문제 14681
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		 int a = s.nextInt();
+         int b = s.nextInt();
+         int c=0;
+        if(a>0) {
+            if(b>0){
+                c=1;
+            }else{
+                c=4;
+            }
+        }else{
+            if(b>0){
+                c=2;
+            }else{
+                c=3;
+            }
+        }
+        System.out.println(c);
+	}
+}
+
