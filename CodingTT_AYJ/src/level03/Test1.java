5a6b15d13729b0a17c3afce2cbe4e76c5e9404a1
ayj8487 <ayj8487@naver.com>
2021-12-16 22:06:30 +0900

    baekjoon 03_01_10950(com)

--- CodingTT_AYJ/src/level03/Test1.java
+++ CodingTT_AYJ/src/level03/Test1.java
@@ -1,13 +1,18 @@
 package level03;
 
+import java.util.Scanner;
 
 public class Test1 {
 
 	//백준코딩 단계3 for문 01문제 2739
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 
-	
+		int i =  s.nextInt(), j;
+		for( j = 1; j < 10; j++ ) {
+			System.out.println(i + " * " + j +" = " + i * j);
+		}
 		
-	}
+		}
 	}
 
---
+++ CodingTT_AYJ/src/level03/Test2.java
@@ -0,0 +1,13 @@
+package level03;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+	//백준코딩 단계3 for문 02문제 10950
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		}
+	}
+
