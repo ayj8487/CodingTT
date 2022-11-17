d8a8e2caed28cfa64546a11b960ddf307a38e2ff
ayj8487 <ayj8487@naver.com>
2022-11-17 14:20:52 +0900

    baekjoon 03-04 25304

--- CodingTT_AYJ/src/level03/Test3.java
+++ CodingTT_AYJ/src/level03/Test3.java
@@ -1,10 +1,23 @@
 package level03;
 
+import java.util.Scanner;
+
 public class Test3 {
 
 	
 	//백준코딩 단계3 for문 03문제 8393
+	// n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
 	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+	
+		int n = sc.nextInt();
+		int sum = 0;
 		
+		for(int i = 1; i<n +1 ; i++) {
+				sum +=i;
+		}
+		
+		System.out.println(sum);
+
 	}
 }
